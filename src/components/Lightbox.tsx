adbfb5cf5beb97eddd24f1649ff4700f75542523
Mars-Reborn <106602563+Mars-Reborn@users.noreply.github.com>
2022-10-27 22:16:44 +0100

    create image lightbox

---
+++ src/components/Lightbox.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Lightbox.css'
+
+const Lightbox = () => {
+  return (
+    <div>
+        <div className='column'></div>
+    </div>
+  )
+}
+
+export default Lightbox
