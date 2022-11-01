7a735d038ddf31fe84b51045a8b08e45deb0700e
Mars-Reborn <106602563+Mars-Reborn@users.noreply.github.com>
2022-11-01 20:53:43 +0100

    add lightbox components

--- src/components/Lightbox.tsx
+++ src/components/Lightbox.tsx
@@ -1,15 +1,37 @@
-import React from 'react'
-import './Lightbox.css'
+import React, { useState } from 'react';
 
+const LightBox = ({ children, src, alt, Wrapper = 'div', zIndex = 100 }) => {
+	const [isOpen, setIsOpen] = useState(false);
 
-const Lightbox = () => {
-  return (
-    <div>
-        <div className='row'>
-            <div className="column"></div>
-        </div>
-    </div>
-  )
-}
+	const toggleIsOpen = () => {
+		setIsOpen(!isOpen);
+	};
 
-export default Lightbox
+	return (
+		<Wrapper onClick={toggleIsOpen}>
+			{children}
+			{isOpen ?
+				<div onClick={toggleIsOpen} style={{
+					position: 'fixed',
+					top: '0',
+					left: '0',
+					height: '100vh',
+					width: '100vw',
+					backgroundColor: 'rgba(0,0,0,0.7)',
+					cursor: 'pointer',
+					zIndex
+				}}>
+					<img src={src}
+						alt={alt}
+						style={{
+							height: 'auto',
+							width: '100%'
+						}}
+					/>
+				</div>
+				: null}
+		</Wrapper>
+	);
+};
+
+export default LightBox;
